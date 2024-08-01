3c1b031afa3c1eb3c11c4a9f0d129042082eccb9
JavierVDG27 <75635305+JavierVDG27@users.noreply.github.com>
2024-08-01 06:35:12 -0600

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProductosComponent } from './productos/productos.component';
 import { CrearProductoComponent } from './crear-producto/crear-producto.component';
+import { RegistrarComponent } from './registrar/registrar.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'productos', component: ProductosComponent },
   { path: 'crearProducto', component: CrearProductoComponent },
+  { path: 'registrar', component: RegistrarComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
